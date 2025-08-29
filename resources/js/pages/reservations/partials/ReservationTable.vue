c25f39c0566f449c06df30be4de6c1b495a7abf4
Emanuel Vaca <emanuelvacaibanez@gmail.com>
2025-08-29 19:12:16 -0400

    Remove edit

--- resources/js/pages/reservations/partials/ReservationTable.vue
+++ resources/js/pages/reservations/partials/ReservationTable.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Pagination from '@/components/common/Pagination.vue';
-import Button from '@/components/ui/button/Button.vue';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Reservation } from '@/models/Reservation';
 
@@ -17,7 +16,7 @@ defineProps<{ reservations: { data: Reservation[]; links: any[]; total: number }
                 <TableHead>End Time</TableHead>
                 <TableHead>Date</TableHead>
                 <TableHead>Status</TableHead>
-                <TableHead>Actions</TableHead>
+                <!-- <TableHead>Actions</TableHead> -->
             </TableRow>
         </TableHeader>
         <TableBody>
@@ -29,7 +28,7 @@ defineProps<{ reservations: { data: Reservation[]; links: any[]; total: number }
                 <TableCell>{{ reservation.event_date_human }}</TableCell>
                 <TableCell>{{ reservation.status }}</TableCell>
                 <TableCell>
-                    <Button>Editar</Button>
+                    <!-- <Button>Editar</Button> -->
                 </TableCell>
             </TableRow>
         </TableBody>
